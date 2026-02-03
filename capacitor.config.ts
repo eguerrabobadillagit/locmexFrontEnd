fc816341d1e11947b1d9570d0574b48961775f73
eguerrabobadillagit <eguerra.bobadillagit@gmail.com>
2026-02-02 19:53:10 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gpsFront',
+  webDir: 'www'
+};
+
+export default config;
